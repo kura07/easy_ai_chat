d0452c574be42f323feadd995521e72a612f9fdb
kura07 <clarinet6132@gmail.com>
2026-04-28 22:23:52 +0900

    a

--- library.js
+++ library.js
@@ -240,7 +240,7 @@ const gemini = {
 
     // fetch
     try {
-      const body = JSON.stringify(({ contents, generationConfig: { temperature, safetyThreshold: "OFF", serviceTier: "flex" } }));
+      const body = JSON.stringify(({ contents, generationConfig: { temperature }, safetyThreshold: "OFF", serviceTier: "flex" }));
       const httpRes = await fetch(`https:/\/generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${gemini._apiKey}`, {
         method: "post", body,
       });
